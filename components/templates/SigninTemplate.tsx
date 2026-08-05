3d8a17fa2df5e7f0b9ed46501e3ddbfbc4b64f31
maker3391 <cjstn3391@gmail.com>
2026-08-05 14:19:49 +0900

    fix: 로그인 페이지 레이아웃 상단 여백 조정

--- components/templates/SigninTemplate.tsx
+++ components/templates/SigninTemplate.tsx
@@ -3,7 +3,7 @@ import { SigninForm } from "@/components/organisms/SigninForm";
 
 export function SigninTemplate() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center px-4 py-12">
+    <main className="-mt-[60px] flex flex-1 flex-col items-center justify-center px-4 py-12">
       <div className="flex w-full max-w-md flex-col gap-10">
         <SigninAuthHeader />
         <SigninForm />

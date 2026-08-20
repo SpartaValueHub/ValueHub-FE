d2ed18cb23654256447c645e3b7cd762cb7281c8
seon022 <seon022@icloud.com>
2026-08-20 14:53:40 +0900

    chore: 개발용 채팅 예약 페이지를 제거
    채팅 연동 전 검증용 /dev/appointment와 전용 플로우 코드를 걷어낸다.

--- app/(header)/dev/appointment/inline/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatAppointmentDevTemplate } from "@/components/templates/dev/ChatAppointmentDevTemplate";
-
-export default function AppointmentInlineDevPage() {
-  return <ChatAppointmentDevTemplate mode="popover-inline" />;
-}
--- app/(header)/dev/appointment/stacked/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatAppointmentDevTemplate } from "@/components/templates/dev/ChatAppointmentDevTemplate";
-
-export default function AppointmentStackedDevPage() {
-  return <ChatAppointmentDevTemplate mode="stacked" />;
-}
